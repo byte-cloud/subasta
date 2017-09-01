5b7355f52463cc4a688caeb48a0afb551c711a40
Akshita0402 <akshitakh0402@gmail.com>
2017-09-01 17:41:43 +0530

    Added basic nav bar

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-console.log("Hello!");
+var express = require('express'),
+    app     = express(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+    
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
-console.log("Hello Beta");
+mongoose.connect("mongodb://localhost/subasta");
+
+app.get('/', function(req, res){
+    res.render("home");
+});
+
+app.listen(3000, function(){
+    console.log("Subasta now listening on port 3000");
+});
