2f6c94f8d9ba5e8c9ae063e90cf5928d26322e12
Akshita0402 <akshitakh0402@gmail.com>
2017-09-01 16:12:48 +0530

    Pulling

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-var express = require('express'),
-    app     = express(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+console.log("Hello!");
 
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-
-mongoose.connect("mongodb://localhost/subasta");
-
-app.get('/', function(req, res){
-    res.send("Its working");
-});
-
-app.listen(3000, function(){
-    console.log("Subasta now listening on port 3000");
-});
+console.log("Hello Beta");
